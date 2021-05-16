4e79fb96326d636092ceb032617c6640a2855ff4
hyomin <gyals6349@gmail.com>
2021-05-16 12:44:31 +0900

    [feat] BOJ 1978 소수찾기

---
+++ Baekjoon/src/p1978.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class p1978 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int n = Integer.parseInt(input.nextLine());
+        boolean[] prime = new boolean[1001];
+        prime[1] = true;
+        for(int i=2;i<=500;i++){
+            int j = 2;
+            while(j*i<=1000){
+                prime[i*j] = true;
+                j++;
+            }
+        }
+        int answer = 0, idx=0;
+        while(idx<n){
+            int x = input.nextInt();
+            if(!prime[x]) answer++;
+            idx++;
+        }
+        System.out.println(answer);
+    }
+}
