d139528204f95b28c4ed08d5e248b04a90b30c2f
hyomin <gyals6349@gmail.com>
2021-05-30 15:48:11 +0900

    [feat] BOJ 15651 N과 M(3)

---
+++ Baekjoon/src/p15651.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class p15651 {
+
+    static int[] selected;
+    static int N,M;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        N = input.nextInt();
+        M = input.nextInt();
+        selected = new int[M];
+
+        perm(1,0);
+        System.out.println(sb);
+    }
+
+    public static void perm(int start ,int select){
+        if(select == M){
+            for(int val : selected){
+                sb.append(val).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+        for(int i=start; i<=N; i++){
+            selected[select] = i;
+            perm(start, select+1);
+        }
+    }
+}
