e22cb28b96f6858603f4c7c8fb467c5ec53b545b
hyomin <gyals6349@gmail.com>
2021-08-09 23:23:35 +0900

    [feat] BOJ 2571 색종이-3 (코드 참고.ver)

--- Baekjoon/src/p2571.java
+++ Baekjoon/src/p2571.java
@@ -1,71 +1,48 @@
 import java.util.*;
 
+//https://zoosso.tistory.com/155
 public class p2571 {
 
-    static boolean[][] map = new boolean[101][101];
-    static int[][] pos;
+    static int[][] map = new int[101][101];
 
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        pos = new int[N][2];
         for(int i=0;i<N;i++){
-            int X = sc.nextInt();
-            int Y = sc.nextInt();
-            pos[i][0] = X;
-            pos[i][1] = Y;
-
-            for(int x=X;x<X+10;x++){
-                for(int y=Y;y<Y+10;y++){
-                    map[x][y] = true;
+            int C = sc.nextInt();
+            int R = sc.nextInt();
+            for(int col=C;col<C+10;col++){
+                for(int row=R;row<R+10;row++){
+                    map[col][row] = 1;
                 }
+            }
+        }
+
+        //세로구간에 대한 누적합 구하기
+        //map[col][row] = 영역의 높이를 담는 배열이 됨
+        //사각형을 찾을 때: 기준점 보다 높은 높이로 갈 수 없음 ???
+        for(int col=1;col<=100;col++){
+            for(int row=1;row<=100;row++){
+                //중간에 비어있을 수도 있어서
+                if(map[col][row]!=0) map[col][row] += map[col-1][row];
             }
         }
 
         int max = 100;
-        for(int i=0;i<N;i++){
-            int ret = Math.max (h(i), w(i));
-            if (max < ret) max = ret;
+
+        //모든 위치를 기준으로 우측으로 확장해가며 최대 사각형을 찾아본다
+        for(int col=1;col<=100;col++){  //height
+            for(int row=1;row<=100;row++){  //width
+                int h = 100;
+                for(int z=row; z<=100; z++){
+                    h = Math.min(h, map[col][z]);   //기준점으로 가장 낮은 높이 구하기
+                    max = Math.max(max, h*(z-row+1)); //높이 * 최대로 확장시켜서 구한 width
+                }
+            }
         }
 
         System.out.println(max);
-    }
-    public static int h(int i){
-        int x = pos[i][0], y = pos[i][1];
-        int low = y, high = y;
-        while(low>0 && map[x][low]) low--;
-        while(high<=100 && map[x][high]) high++;
-        int height = high - low - 1;
-        int min = 0;
 
-        loop:for(int j=x+1;j<=100;j++){
-            for(int k=low+1; k<high; k++){
-                if(!map[j][k]){
-                    min = j;
-                    break loop;
-                }
-            }
-        }
-        return height*(min-x);
-    }
-
-    public static int w(int i){
-        int x = pos[i][0], y = pos[i][1];
-        int low = x, high = x;
-        while(low>0 && map[low][y]) low--;
-        while(high<=100 && map[high][y]) high++;
-        int weight = high - low - 1;
-
-        int min = 0;
-        loop:for(int j=y+1;j<=100;j++){
-            for(int k=low+1; k<high; k++){
-                if(!map[k][j]){
-                    min = j;
-                    break loop;
-                }
-            }
-        }
-        return weight*(min-y);
     }
 
 }
