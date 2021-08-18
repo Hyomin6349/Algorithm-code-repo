d2c928c0d1e6407edb2f05ae350ab95a736bc0a3
hyomin <gyals6349@gmail.com>
2021-08-18 22:23:34 +0900

    [feat] BOJ 1328 고층 건물 dp.ver

--- Baekjoon/src/p1328.java
+++ Baekjoon/src/p1328.java
@@ -1,49 +1,36 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class p1328 {
 
-    static int D = 1000000007;
-    static long[][] memo = new long[101][101]; //시간 초과 땜시
+    static long[][][] dp;
+    static int div = 1000000007;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int L = sc.nextInt();
-        int R = sc.nextInt();
+    public static void main(String[] args) throws Exception{
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        if(L-1+R > N || L+R==2) {
-            System.out.println(0);
-            System.exit(0);
+        int n = Integer.parseInt(st.nextToken());
+        int l = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+
+        dp = new long[n+1][n+1][n+1];
+
+        solve(n,l,r);
+        System.out.println(dp[n][l][r]);
+
+    }
+    static void solve(int n, int l, int r) {
+        dp[1][1][1] = 1;
+        for (int i = 2;i< n+1; i++) {
+            dp[i][i][1] = dp[i][1][i] = 1;
+            for (int j = 1; j< l+1; j++) {
+                for (int k = 1; k< r+1; k++) {
+                    dp[i][j][k] = (dp[i - 1][j][k-1] +dp[i - 1][j - 1][k]
+                            + (dp[i - 1][j][k] * (i-2)))%div;
+                }
+            }
         }
-
-        // (오른쪽, 왼쪽에 놓을 건물 선택) * (나머지 건물들 줄세우기) * (중간의 큰 건물들 자리 고르기)
-        // (L+R-2 C R-1) * (N-R-L+1)! * (N-R-L+2 H R+L-3)
-        // N-R-L+2 H R+L-3 = N-2 C R+L-3 => nHr = n+r-1Cr
-
-        long res = comb(L+R-2, R-1) % D;
-        //System.out.println(res + " " +(N-R-L+1));
-        res = (res * fact(N-R-L+1)%D) % D;
-        //System.out.println(comb(N-2, R+L-3));
-        if(N-2>0 && R+L-3>=0) res = (res * comb(N-2, R+L-3)%D) % D;
-
-        System.out.println(res);
-
     }
-
-    public static long comb(int n, int r) {
-        if(memo[n][r]!=0) return memo[n][r];
-        if(n==r || r==0) {
-            memo[n][r] = 1;
-            return 1;
-        }
-
-        memo[n][r] = (comb(n-1, r-1)%D + comb(n-1, r)%D)%D;
-        return memo[n][r];
-    }
-
-    public static long fact(int n) {
-        if(n==1 || n==0) return 1;
-        return (n*(fact(n-1)%D))%D; //1000000007
-    }
-
 }
