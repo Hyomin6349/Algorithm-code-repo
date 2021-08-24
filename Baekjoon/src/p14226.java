d9af38d5e4787f4653312362072b75fde5a5fcbe
hyomin <gyals6349@gmail.com>
2021-08-24 23:11:31 +0900

    [feat] BOJ 14226 이모티콘 success.ver

--- Baekjoon/src/p14226.java
+++ Baekjoon/src/p14226.java
@@ -1,54 +1,51 @@
 import java.util.*;
 
 public class p14226 {
-    static int S;
+
+    static int S, min = Integer.MAX_VALUE;
+    static boolean[][] visit = new boolean[2002][2002];
+
+    public static class State{
+        int monitor, copy, time;
+        public State(int m, int c, int t){
+            monitor = m;
+            copy = c;
+            time = t;
+        }
+    }
 
     public static  void main(String[] args){
         Scanner sc = new Scanner(System.in);
         S  = sc.nextInt();
-        int INF = 987654321;
-
-        int[][] memo = new int[1001][1001];
-        for(int i=1;i<=1000;i++){
-            for(int j=0;j<=1000;j++) memo[i][j] = INF;
-        }
-
-        memo[1][0] = 0;
-        memo[1][1] = 1;
-
-        for(int i=2; i<=1000;i++){ //i: i개의 이모티콘을 작성할 최소 수
-            int min = INF;
-            int copy = -1;
-            for(int j=1; j<i;j++){ //j: 화면에 작성된 이모티콘 수
-                for(int k=0;k<j;k++){ //k: 클립보드에 저장된 이모티쿈 수
-                    if(j+k==i && memo[i][k]>memo[j][k]+1){
-                        memo[i][k] = memo[j][k]+1; //붙여넣기 한번
-                    }
-                    if(j+j==i && memo[i][j]>memo[j][k]+2){
-                        memo[i][j] = memo[j][k]+2; //j 복사 붙여넣기
-                    }
-                    if(min>memo[i][k]){
-                        min = memo[i][k]; //i개의 이모티콘을 만드는 경우의 수 중 최소 수
-                        copy = k;
-                    }
-                }
-            }
-            memo[i][i] = min+1;
-            if(copy!=-1 && memo[i-1][copy] > min) memo[i-1][copy] = min;
-        }
-
-//        int min = INF;
-//        for(int i=0;i<=1000;i++){
-//            if(min>memo[S][i]) min = memo[S][i];
-//        }
-//        System.out.println(min);
-
-        for(int i=1;i<=20;i++){
-            for(int j=0; j<i; j++){
-                System.out.print(memo[i][j]+" ");
-            }
-            System.out.println();
-        }
+        bfs();
     }
 
+    public static void bfs(){ //가장 최소 시간 찾기
+        Queue<State> q = new LinkedList<>();
+        q.add(new State(1, 0, 0));
+
+        while(!q.isEmpty()){
+            State cur = q.poll();
+            if(visit[cur.monitor][cur.copy]) continue;
+            visit[cur.monitor][cur.copy] = true;
+            if(cur.monitor == S) min = Math.min(min, cur.time);
+
+            if(cur.monitor < S){
+                if(!visit[cur.monitor][cur.monitor]){
+                    q.add(new State(cur.monitor, cur.monitor, cur.time+1));
+                }
+                if(cur.copy!=0 && !visit[cur.monitor+cur.copy][cur.copy]) {
+                    q.add(new State(cur.monitor + cur.copy, cur.copy, cur.time + 1));
+                }
+                if(cur.monitor>1 && !visit[cur.monitor-1][cur.copy]){
+                    q.add(new State(cur.monitor-1, cur.copy, cur.time+1));
+                }
+            }
+            else if(cur.monitor>1 && !visit[cur.monitor-1][cur.copy]) {
+                q.add(new State(cur.monitor - 1, cur.copy, cur.time + 1));
+            }
+        }
+
+        System.out.println(min);
+    }
 }
