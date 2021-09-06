f288a5ec96077d3e68bc1749ffca585cb16fa17c
hyomin <gyals6349@gmail.com>
2021-09-06 22:39:41 +0900

    [update] 카카오 블라인드 테스트 2020 기둥과 보 설치

--- KakaoBlind/src/_2020_기둥과보설치.java
+++ KakaoBlind/src/_2020_기둥과보설치.java
@@ -1,51 +1,42 @@
 import java.util.*;
 
-//정확성 33.6
+//정확성 100
 public class _2020_기둥과보설치 {
 
-    public static  int[][] solution(int n, int[][] build_frame) {
+    static boolean[][] pillars;
+    static boolean[][] pans;
 
-        boolean[][] pillars = new boolean[n][n+1];
-        boolean[][] pans = new boolean[n+1][n];
+    public static int[][] solution(int n, int[][] build_frame) {
+
+        pillars = new boolean[n][n+1];
+        pans = new boolean[n+1][n];
 
         for(int[] build : build_frame){
             int x = build[0];
             int y = build[1];
             if(build[2]==1){
                 if(build[3]==1){ //x,y에 보설치
-                    if(pillars[y-1][x+1] || pillars[y-1][x]) pans[y][x] = true; //한쪽 끝이 기둥이거나
-                    else if(0<x && x+1<n && pans[y][x-1] && pans[y][x+1]) pans[y][x] = true; //양쪽이 보인 경우
-                    else System.out.println(x+" "+y+" 보 설치 실패");
-                    System.out.println(x+" "+y+" 보 설치 성공  "+pans[y][x]);
+                    if(panBuild(n, x, y)) pans[y][x] = true;
+                    else continue;
                 }
                 if(build[3]==0){ //x,y 의 보 삭제
-                    if(0<x && pans[y][x-1] && !pillars[y-1][x-1]) continue;
-                    else if(x+1<n && pans[y][x+1] && !pillars[y-1][x+1]) continue;
-                    else if(y<n && pillars[y][x]){
-                        if(!((0<x && pans[y][x-1]) || pillars[y-1][x])) continue;
-                        if(!(pillars[y-1][x+1] || (x+1<n && pans[y][x+1]))) continue;
-                    }
                     pans[y][x] = false;
-                    System.out.println(x+" "+y+" 보 삭제 성공");
+                    if(y<n && pillars[y][x] && !pillarBuild(n, x, y)) pans[y][x] = true;
+                    if(y<n && x+1<=n && pillars[y][x+1] && !pillarBuild(n, x+1, y)) pans[y][x] = true;
+                    if(0<x && pans[y][x-1] && !panBuild(n, x-1, y)) pans[y][x] = true;
+                    if(x+1<n && pans[y][x+1] && !panBuild(n, x+1, y)) pans[y][x] = true;
                 }
             }
             if(build[2]==0){
                 if(build[3]==1){ // x,y에 기둥 설치
-                    if(y==0) pillars[y][x] = true; //바닥인 경우
-                    else if(pillars[y-1][x]) pillars[y][x] = true; //기둥인 경우
-                    else if(pans[y][x]) pillars[y][x] = true; //보인경우
-                    else if(x-1>=0 && pans[y][x-1]) pillars[y][x] = true;
-                    else System.out.println(x+" "+y+" 기둥 설치 실패");
-                    System.out.println(x+" "+y+" 기둥 설치 성공  "+pillars[y][x]);
+                    if(pillarBuild(n, x, y)) pillars[y][x] = true;
+                    else continue;
                 }
                 if(build[3]==0){ //x,y의 기둥 삭제
-                    if(x<n && pans[y][x] && !pillars[y][x+1]) continue; //기둥 삭제 실패
-                    else if(x-1>=0 && pans[y][x-1] && !pillars[y][x-1]) continue;
-                    else if(y+1<n && pillars[y+1][x]){ //위에 기둥이 있는데
-                        if(!((x<n && pans[y][x]) || (x-1>=0 && pans[y][x-1]))) continue; //받쳐주는 보가 없을 때
-                    }
                     pillars[y][x] = false;
-                    System.out.println(x+" "+y+" 기둥 삭제 성공");
+                    if(y+1<n && pillars[y+1][x] && !pillarBuild(n, x, y+1)) pillars[y][x] = true;
+                    if(x<n && pans[y+1][x] && !panBuild(n, x, y+1)) pillars[y][x] = true;
+                    if(x>0 && pans[y+1][x-1] && !panBuild(n, x-1, y+1)) pillars[y][x] = true;
                 }
             }
         }
@@ -79,9 +70,27 @@ public class _2020_기둥과보설치 {
         return answer;
     }
 
+    public static boolean panBuild(int n, int x, int y){
+        System.out.println(x+" "+y);
+        if(pillars[y-1][x+1] || pillars[y-1][x]) return true;
+        else if(0<x && x+1<n && pans[y][x-1] && pans[y][x+1]) return true;
+        else return false;
+    }
+
+    public static boolean pillarBuild(int n, int x, int y){
+        if(y==0)  return true;
+        else if(pillars[y-1][x]) return true; //기둥인 경우
+        else if(pans[y][x]) return true; //보인경우
+        else if(x-1>=0 && pans[y][x-1]) return true;
+        else return false;
+    }
+
     public static void main(String[] args) {
         int n = 5;
-        int[][] build_frames = {{1,0,0,1},{1,1,1,1},{2,1,0,1},{2,2,1,1},{5,0,0,1},{5,1,0,1},{4,2,1,1},{3,2,1,1}};
-        solution(n, build_frames);
+        int[][] build_frames = {{0,0,0,1},{2,0,0,1},{4,0,0,1},{0,1,1,1},{1,1,1,1},{2,1,1,1},{3,1,1,1},{2,0,0,0}, {1,1,1,0}, {2,2,0,1}};
+        int[][] answer = solution(n, build_frames);
+        for(int i=0;i<answer.length;i++){
+            System.out.println(Arrays.toString(answer[i]));
+        }
     }
 }
