1f8a73dde800c466b95a8b73120fb8b8d315d314
hyomin <gyals6349@gmail.com>
2021-10-13 00:11:22 +0900

    [feat] BOJ 14425 문자열 집합 HashCode.ver

--- Baekjoon/src/p14425.java
+++ Baekjoon/src/p14425.java
@@ -3,67 +3,25 @@ import java.io.*;
 
 public class p14425 {
 
-    public static class Trie{
-        int length;
-        Trie[] child;
-
-        public Trie(int length){
-            this.length = length;
-            child = new Trie[26];
-        }
-
-        public void insert(char[] word){
-            Trie cur = this;
-            next: for(char c: word){
-                if(cur.child[c-'a']!=null) {
-                    cur = cur.child[c-'a'];
-                    continue next;
-                } else {
-                    cur.child[c-'a'] = new Trie(0);
-                    cur = cur.child[c-'a'];
-                }
-            }
-        }
-
-        public int find(char[] word){
-            Trie cur = this;
-            next: for(char c: word){
-                if(cur.child[c-'a']==null) return 0;
-
-                cur = cur.child[c-'a'];
-                continue next;
-            }
-            return 1;
-        }
-    }
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        Trie[] root = new Trie[N];
+        Set<Integer> set = new HashSet<>();
 
         for(int i=0;i<N;i++){
-            char[] word = br.readLine().toCharArray();
-            root[i] = new Trie(word.length);
-            root[i].insert(word);
+            String word = br.readLine();
+            int hc = word.hashCode();
+            set.add(hc);
         }
 
         int cnt = 0;
         for(int i=0;i<M;i++){
-            char[] word = br.readLine().toCharArray();
-
-            for(int j=0;j<N;j++){
-                if(word.length == root[j].length) {
-                    int res = root[j].find(word);
-                    if (res == 1) {
-                        cnt++;
-                        break;
-                    }
-                }
-            }
+            String word = br.readLine();
+            int hc = word.hashCode();
+            if(set.contains(hc)) cnt++;
         }
 
         System.out.println(cnt);
