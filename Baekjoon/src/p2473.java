e3bc5e039ce0fc8c76a6b4d4b090bbe2f2b2f3a4
hyomin <gyals6349@gmail.com>
2021-11-04 22:30:09 +0900

    [feat] BOJ 2473 세 용액

---
+++ Baekjoon/src/p2473.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class p2473 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = Integer.parseInt(sc.nextLine());
+        long[] arr = new long[N];
+        StringTokenizer st = new StringTokenizer(sc.nextLine());
+        for(int i=0;i<N;i++){
+            arr[i] = Long.valueOf(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        long abs = 3000000000L;
+        long[] ans = new long[3];
+        for(int i=0;i<N;i++){
+            long ch = arr[i];
+            int left = i+1;
+            int right = N-1;
+
+            while(left<right){
+                if(arr[left]==ch) left++;
+                else if(arr[right]==ch) right--;
+                else{
+                    long val = arr[left] + arr[right] + ch;
+                    if(Math.abs(val) < abs){
+                        abs = Math.abs(val);
+                        ans[0] = ch;
+                        ans[1] = arr[left];
+                        ans[2] = arr[right];
+                    }
+                    if(val > 0) right--;
+                    else if(val<=0) left++;
+                }
+            }
+        }
+
+        System.out.println(ans[0]+" "+ans[1]+" "+ans[2]);
+    }
+}
